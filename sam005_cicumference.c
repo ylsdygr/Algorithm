a3ad32716feb4e4b80ba693985abbaaad8800101
ylsdygr <ylsdygr@gmail.com>
2017-08-23 10:16:36 +0800

    C Language Basic
    basic operator usage

---
+++ sam005_cicumference.c
@@ -0,0 +1,16 @@
+//
+// Created by zww on 27/07/17.
+//
+/*
+ * 计算正方形的周长
+ */
+
+#include <stdio.h>
+
+int main() {
+    int a, cicum;
+    a = 4;
+    printf("正方形的周长是: %d", a * 4);
+    printf("\n");
+    return 0;
+}
