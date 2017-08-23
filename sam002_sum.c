1c01622474c62081266dbe9e86ba6ec09fb8f849
ylsdygr <ylsdygr@gmail.com>
2017-08-23 10:01:31 +0800

    C Language Basic
    basic function sum with c language

---
+++ sam002_sum.c
@@ -0,0 +1,16 @@
+//
+// Created by zww on 17-7-27.
+//
+/*
+ * 一个完整的C程序求和
+ */
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    printf("求和的结果为：%d\n",sum);
+    printf("求和的结果为：%d\n",a + b);
+    return 0;
+}
